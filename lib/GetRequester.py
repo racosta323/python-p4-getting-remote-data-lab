67a5ba447a9af4f9757a208f041bcdc6eb5349e5
Rene <racosta323@gmail.com>
2024-03-17 12:24:38 -0500

    lab complete

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,25 @@
 import requests
 import json
+import ipdb
 
 class GetRequester:
 
     def __init__(self, url):
         self.url = url
 
+    def get_response(self):
+        response = requests.get(self.url)
+        return response
+
     def get_response_body(self):
-        pass
+        response_body = self.get_response()
+        #.content gets body of resp
+        return response_body.content
 
     def load_json(self):
-        pass
+        json_response = self.get_response().json()
+        return json_response
+        # return json.dumps(json_response, indent=1)
+
+   
+# ipdb.set_trace()
